95ec2480e2b9574f1e2d20aa816cd6269039083e
Tatsiana Gelahova <tgelahova99@gmail.com>
2019-02-10 18:49:46 +0300

    feature: add footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoginComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
