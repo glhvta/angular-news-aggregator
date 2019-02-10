a42f6b0b53d3f28e86355abb44f3d314274ffa75
Tatsiana Gelahova <tgelahova99@gmail.com>
2019-02-10 18:16:07 +0300

    feature: add login component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  isLogged: boolean = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  handleLogin(): void {
+    console.log('Log in action');
+  }
+
+  handleLogout(): void {
+    console.log('Log out action');
+  }
+}
