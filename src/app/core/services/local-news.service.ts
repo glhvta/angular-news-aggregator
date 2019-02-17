eee464b07d114f55816b139d1a307c246c115f17
Tatsiana Gelahova <tgelahova99@gmail.com>
2019-02-17 23:10:04 +0300

    feature: fetch article by id from server

--- src/app/core/services/local-news.service.ts
+++ src/app/core/services/local-news.service.ts
@@ -12,43 +12,30 @@ import { LOCAL_NEWS } from '../constants/newsProviders';
 })
 export class LocalNewsService implements NewsProvider {
   private type = LOCAL_NEWS;
-  private articles: Article[] = [];
-
   private requestEndpoint = 'http://localhost:3000/local-news';
 
   constructor(private http: HttpClient) {}
 
   public getNews(): Observable<Article[]> {
     return this.http.get(this.requestEndpoint).pipe(
-      tap(this.updateArticles),
-      map(() => this.localArticles),
-      // map(() => this.webArticles),
+      map(this.addProviderType),
       // catchError(console.warn),
     );
   }
 
   public getArticle(id: string): Observable<Article> {
-    const article = this.articles.find(({ _id }) => _id === id);
-
-    return of(article);
+    return this.http
+      .get<Article>(`${this.requestEndpoint}/${id}`)
+      .pipe(tap(data => console.log('article local: ', data)));
   }
 
   public createArticle(article: Article): Observable<Article> {
-
     return this.http
       .post<Article>(this.requestEndpoint, { article })
       .pipe(tap(() => console.log('post request was done')));
   }
 
-  // TODO: create base service with common methods
-  private updateArticles = (articles: Article[]): void => {
-    this.articles = [
-      ...this.articles,
-      ...articles.map(data => ({ ...data, provider: this.type })),
-    ];
-  }
-
-  private get localArticles(): Article[] {
-    return this.articles;
+  private addProviderType = (articles: Article[]): Article[] => {
+    return articles.map(data => ({ ...data, provider: this.type }));
   }
 }
